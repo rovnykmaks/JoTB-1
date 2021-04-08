d02d164c75435a47e97d78bfeb78f97ff4540f3e
rovnykmaks <60928246+rovnykmaks@users.noreply.github.com>
2021-04-08 17:05:33 +0300

    Starting project

---
+++ src/main/java/com/github/rovnykmaks/telegramBotjournal/TelegramBotJournalApplication.java
@@ -0,0 +1,13 @@
+package com.github.rovnykmaks.telegramBotjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotJournalApplication.class, args);
+	}
+
+}
